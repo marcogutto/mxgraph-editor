130b871e1a99ffdf7ce99ace75adad5776eb24b0
Marco Seabra <marcogutto@gmail.com>
2019-07-11 15:44:29 -0300

    Implementando o Hello World!

---
+++ angular/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements AfterViewInit {
+
+  @ViewChild('graphContainer', {static: false}) graphContainer: ElementRef;
+
+  ngAfterViewInit() {
+    const graph = new mxGraph(this.graphContainer.nativeElement);
+
+    try {
+      const parent = graph.getDefaultParent();
+      graph.getModel().beginUpdate();
+
+      const vertex1 = graph.insertVertex(parent, '1', 'Vertex 1', 0, 0, 200, 80);
+      const vertex2 = graph.insertVertex(parent, '2', 'Vertex 2', 0, 0, 200, 80);
+
+      graph.insertEdge(parent, '', '', vertex1, vertex2);
+    } finally {
+      graph.getModel().endUpdate();
+      new mxHierarchicalLayout(graph).execute(graph.getDefaultParent());
+    }
+  }
+
+}
